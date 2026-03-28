e6f3b11172c02839f7080c5404ea13cf51e7150d
surajsinghsft <surajsinghsft@gmail.com>
2026-03-28 13:44:56 +0530

    update project

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/function";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
+
+// ✅ VERY IMPORTANT (build error fix)
+export const dynamic = "force-dynamic";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [generateIndustryInsights],
 });
-z
