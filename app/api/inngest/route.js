f6dea3c4bb9dc09294c72601cc7831c20ff4cfcc
surajsinghsft <surajsinghsft@gmail.com>
2026-03-28 13:53:01 +0530

    update project

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/function";
 
 // ✅ VERY IMPORTANT (build error fix)
 export const dynamic = "force-dynamic";
